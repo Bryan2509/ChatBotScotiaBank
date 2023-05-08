b0370b171094db9798f4aec2fc18c8c4b14b2bab
Bryan <bhuertasc@uni.pe>
2023-05-08 10:33:50 -0500

    commit gosu :v

--- actions/actions.py
+++ actions/actions.py
@@ -11,10 +11,13 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from threading import Thread
 import requests
 import sqlite3
 import os
 import openai
+import json
  
 import pandas as pd
 import openpyxl 
@@ -23,9 +26,9 @@ import openpyxl
 #
 #
 
-openai.api_key = os.environ["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"]
+#openai.api_key = os.environ["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"]
 
-class ActionChatGPT(Action):
+"""class ActionChatGPT(Action):
 
     def name(self) -> Text:
         return "action_chat_gpt"
@@ -47,20 +50,27 @@ class ActionChatGPT(Action):
 
         chat_gpt_response = response.choices[0].text.strip()
         dispatcher.utter_message(text=chat_gpt_response)
-        return []
+        return [] """
 
 #GUARDA EL NOMBRE PARA PODER ALMACENARLO EN LA BASE DE DATOS
 class ActionGuardarNombre(Action):
     def name(self) -> Text:
         return "action_guardar_nombre"
     
-    def run(self, dispatcher: CollectingDispatcher,
-           tracker: Tracker,
-           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-       print(tracker.get_slot('nombre'))
-       dispatcher.utter_message(response = "utter_como_te_ayudo")
-       return [SlotSet('nombre',tracker.latest_message['text'])]
-   
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict) -> List[Dict[Text, Any]]:
+        
+        nombre = tracker.get_slot('nombre')
+        print(nombre)
+        
+        if nombre is not None:
+            mensaje = f"Hola, {nombre}! un gusto conocerte"
+        else:
+            mensaje = "Hola! un gusto conocerte"
+            
+        dispatcher.utter_message(text = mensaje)
+        
+        return [SlotSet("nombre", str.upper(nombre))]
+    
 #GUARDA EL PRODUCTO PARA PODER GUARDARLO EN LA BASE DE DATOS
 class ActionGuardarProducto(Action):
     def name(self) -> Text:
@@ -69,9 +79,13 @@ class ActionGuardarProducto(Action):
     def run(self, dispatcher: CollectingDispatcher,
            tracker: Tracker,
            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-       print(tracker.get_slot('producto'))
-       dispatcher.utter_message(response = "action_dar_info_producto")
-       return [SlotSet('producto',tracker.latest_message['text'])]
+        
+        producto = tracker.latest_message['text']
+        print(producto)
+        
+        mensaje = f"{str.upper(producto)} es: "
+        dispatcher.utter_message(text = mensaje)
+        return [SlotSet('producto', str.upper(producto))]
 
 #GUARDA EL CONCEPTO PARA PODER GUARDARLO EN LA BASE DE DATOS
 class ActionGuardarConcepto(Action):
@@ -81,9 +95,14 @@ class ActionGuardarConcepto(Action):
     def run(self, dispatcher: CollectingDispatcher,
            tracker: Tracker,
            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-       print(tracker.get_slot('concepto'))
-       dispatcher.utter_message(response = "action_dar_info_concepto")
-       return [SlotSet('concepto',tracker.latest_message['text'])]
+        
+        concepto = tracker.latest_message('Text')
+        print(concepto)
+        
+        mensaje = f"{str.upper(concepto)} es: "
+        dispatcher.utter_message(text = mensaje)
+        
+        return [SlotSet('concepto',str.upper(concepto))]
    
 #
 def datastore(nombre, producto, concepto):
@@ -107,53 +126,74 @@ class ActionStore(Action):
         concepto=tracker.get_slot('concepto')
         
         datastore(nombre, producto, concepto)
-        dispatcher.utter_message("Gracias a ti!!")
+        #dispatcher.utter_message("Gracias a ti!!")
         print(nombre, producto, concepto)
         return []
     
-class dar_info_producto(Action):
+class recuperar_info_producto(Action):
     
     def name(self) -> Text:
-        return "action_dar_info_producto"
+        return "action_recuperar_info_producto"
     
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
+        producto = tracker.get_slot('producto')
+        
         df = pd.read_excel("BBDD Scotiabank.xlsx", engine='openpyxl', sheet_name='Productos')
-        x = tracker.get_slot('producto')
-        x = str(x)
-        df = df[df['Concepto'].str.lower() == x]
-        df = df['Descripcion']
-        print(df)
-
-        #df = dict(df)
-        dispatcher.utter_message(text = df)
-        return []
     
+        df = df[df['PRODUCTO'].str.upper() == str.upper(producto)]
+        
+        if df is not None:
+            df = df['DETALLE'].to_json()
+            numero = df[2:4]
+            json_obj = json.loads(df)
+            descripcion = json_obj[numero]
+        else:
+            descripcion = "No hay informacion"
+        
+        print(descripcion)
+        
+        return [SlotSet('descripcion',descripcion)]
     
-
-class dar_info_concepto(Action):
+class dar_info(Action):
     
     def name(self) -> Text:
-        return "action_dar_info_concepto"
+        return "action_dar_info"
     
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        descripcion = tracker.get_slot('descripcion')
+        print(descripcion)
+        dispatcher.utter_message(text = descripcion)
+        return []
+    
+class recuperar_info_concepto(Action):
+    
+    def name(self) -> Text:
+        return "action_recuperar_info_concepto"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        concepto = tracker.get_slot('concepto')
         
         df = pd.read_excel("BBDD Scotiabank.xlsx", engine='openpyxl', sheet_name='Terminos')
         
-        x = tracker.get_slot('concepto')
-        x = str(x)
-        df = df[df['Concepto'].str.lower() == x]
-        df = df['Descripcion']
-        print(df)
+        df = df[df['CONCEPTO'].str.upper() == str.upper(concepto)]
         
-        dispatcher.utter_message(text = df)
-        return []
+        if df is not None:
+            df = df['DETALLE'].to_json()
+            numero = df[2:4]
+            json_obj = json.loads(df)
+            descripcion = json_obj[numero]
+        else:
+            descripcion = "No hay informacion"
     
-    
-    
+        print(descripcion)
 
-
+        return [SlotSet('descripcion',descripcion)]
